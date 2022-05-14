feea04682a59ca8339fa964aeaa5581515e1e7da
Sachin Sudheer <sachin.sudheer301@gmail.com>
2022-05-14 17:09:54 +0530

    respose success

--- src/main/scala/hcl/AkkaApi.scala
+++ src/main/scala/hcl/AkkaApi.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import framework.dao.JdbcDao
 //import org.apache.spark.sql.SparkSession
@@ -21,11 +21,10 @@ object AkkaApi extends App {
 
     val simpleRoute =
       path("api" / "myEndpoint") {
-        get 
+        get
           parameter('stock) { stockname: String =>
             val jdbcDao: JdbcDao = new JdbcDao()
-            println(jdbcDao.historicRead(stockname))
-            complete(StatusCodes.OK)
+            complete(HttpResponse(status = StatusCodes.OK, entity = jdbcDao.historicRead(stockname)))
           }
       }
     Http().bindAndHandle(simpleRoute, "localhost", 8080)
